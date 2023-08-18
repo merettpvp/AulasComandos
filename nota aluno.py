e52415ce5202adf86a381dc7a2c275ca2c7ed5cc
merettpvp <marcoantoniorochamerett@gmail.com>
2023-08-18 20:17:08 -0300

    atualizaçao

---
+++ nota aluno.py
@@ -0,0 +1,14 @@
+nota = int(input('Digite a nota do aluno:'))
+
+if nota >= 90:
+ print ('A - excelente')
+elif nota >= 80: 
+ print ('B - Muito bom')
+elif nota >= 70:
+ print ('C - Bom ')
+elif nota >= 60:
+ print ('D - Regular')
+else:
+ print ('Você está reprovado')
+
+
