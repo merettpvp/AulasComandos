0e0bcf35bfae8aef4b35d147ede3ca8dc66ac6b2
merettpvp <@marcoantoniorochamerett@gmail.com>
2023-09-01 20:21:09 -0300

    teste

---
+++ aula 1 de setembro.py
@@ -0,0 +1,10 @@
+opcao = int(input("escolha uma opção:\n1 - Pera\n 2 - Alface\n3 - Caqui\nopção")) 
+
+if opcao ==1:
+	print ("Você escolheu Pera.")
+elif opcao ==2:
+	print ("você escolheu Alface.")
+elif opcao ==3:
+	print ("Você escolheu Caqui.")
+else:
+	print ("Opção inválida.")
